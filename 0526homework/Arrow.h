d7a99a57b3fcb90c788fbd691ee9711e8c9fe353
notebook <ljh1642@gmail.com>
2023-05-30 10:18:47 +0900

    final

--- 0526homework/Arrow.h
+++ 0526homework/Arrow.h
@@ -2,9 +2,9 @@
 
 class Arrow
 {
+	int arrows[10] = { 0 };
 public:
 	void SetArrows();
 	void CheckArrows(int& index, int& typedKey);
 	void PrintArrows(int& index);
-	int arrows[10] = { 0 }; // 멤버변수를 private 상태로 사용하는법..?
 };
--- 0526homework/Player.h
+++ 0526homework/Player.h
@@ -3,7 +3,7 @@ class Player
 {
 public:
 	int arrow = 4;
-	int arrowsIndex = 0; // 멤버변수를 private 상태로 사용하는법..?
+	int arrowsIndex = 0;
 	int isKeyDown(int key);
 	void PlayerArrow();
 };
